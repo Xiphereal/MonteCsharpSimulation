b9d028a7bc7fcf15e3a857e7c4abd96ef7000086
Xiphereal <lozanojalex@gmail.com>
2024-11-14 20:37:13 +0100

    test: split tests by SUT

--- Tests/ThoughputPerDayTests.cs
+++ Tests/ThoughputPerDayTests.cs
@@ -3,26 +3,11 @@ using FluentAssertions.Extensions;
 
 namespace MonteCsharpSimulation.Tests
 {
-    public class SADfasdf
+    public class ThoughputPerDayTests
     {
         // Given the throughput per date and 1 task, a random day throughput is chosen as completion.
         // Given the throughput per date and 2 tasks, their completion is the sum of any two random days throughput.
         // A number of iterations can be specified. There are several possible completion dates.
-
-        [Test]
-        public void MyTestMethod()
-        {
-            Simulation
-                .From(historic: [1.February(2014)])
-                .Should().BeEquivalentTo(
-                [
-                    new Completion()
-                    {
-                        When = 1.February(2014),
-                        Occurrences = 1,
-                    }
-                ]);
-        }
 
         [Test]
         public void ThroughputForSingleDate()
