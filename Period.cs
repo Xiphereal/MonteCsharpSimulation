f273c7bac187276f74ac0c527eb885e030278317
Xiphereal <lozanojalex@gmail.com>
2024-11-14 21:51:21 +0100

    refactor: add Period preconditions

--- Period.cs
+++ Period.cs
@@ -1,8 +1,29 @@
 ﻿
 namespace MonteCsharpSimulation
 {
-    public record Period(
-        DateTime From,
-        DateTime To,
-        IEnumerable<DateTime> TasksCompletionDates);
+    public record Period
+    {
+        public Period(
+            DateTime From,
+            DateTime To,
+            IEnumerable<DateTime> TasksCompletionDates)
+        {
+            if (From > To)
+                throw new ArgumentException();
+
+            if (!TasksCompletionDates.Any())
+                throw new ArgumentException();
+
+            if (TasksCompletionDates.Any(x => x.Date < From || x.Date > To))
+                throw new ArgumentException();
+
+            this.From = From;
+            this.To = To;
+            this.TasksCompletionDates = TasksCompletionDates;
+        }
+
+        public DateTime From { get; }
+        public DateTime To { get; }
+        public IEnumerable<DateTime> TasksCompletionDates { get; }
+    }
 }
